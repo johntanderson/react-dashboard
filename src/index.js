d03e7aded30a488bee0ec8c38438d509513c7d14
John Anderson <john@jtanderson.dev>
2022-02-28 01:48:49 +0000

    Add sidebar feature.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import "@fontsource/source-sans-pro"
+import "@fontsource/source-sans-pro/600.css"
 
 ReactDOM.render(
   <React.StrictMode>
