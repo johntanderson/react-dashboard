cd096f8f394b4286ee12f42f462e08f88ace9103
John Anderson <john@jtanderson.dev>
2022-03-22 21:42:04 +0000

    Add active users chart to homepage.

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import FeaturedInfo from '../../components/FeaturedInfo/FeaturedInfo';
+import Chart from '../../components/Chart/Chart';
 import './home.css';
+import { userData } from '../../DummyData';
 
 export default function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart data={userData} title="Active Users" dataKey="name" grid />
     </div>
   )
 }
